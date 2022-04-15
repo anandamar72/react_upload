2aa0030b3a0ddd917f5e5779f568cb39944daec8
Anand Amar <2019kuec2035@iiitkota.ac.in>
2022-04-15 22:42:59 +0530

    Updates

---
+++ precache-manifest.fd3dbb141196405fe77513c5dd52f19d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9cb6ded0089a916a0f8b",
+    "url": "/react_upload/static/css/main.514e2df2.chunk.css"
+  },
+  {
+    "revision": "9cb6ded0089a916a0f8b",
+    "url": "/react_upload/static/js/main.751aae4d.chunk.js"
+  },
+  {
+    "revision": "ddf21f79c415e7075c02",
+    "url": "/react_upload/static/js/runtime~main.e467f5b8.js"
+  },
+  {
+    "revision": "5ec690ef9274010fe68c",
+    "url": "/react_upload/static/css/2.4607221e.chunk.css"
+  },
+  {
+    "revision": "5ec690ef9274010fe68c",
+    "url": "/react_upload/static/js/2.8ebd595e.chunk.js"
+  },
+  {
+    "revision": "f1488dbc29308337b20f",
+    "url": "/react_upload/static/js/3.8e0b6ac1.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react_upload/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "2700c3573963af6541a39f6145a2c8e4",
+    "url": "/react_upload/index.html"
+  }
+];
